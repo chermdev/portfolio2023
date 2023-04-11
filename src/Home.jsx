caaacdf07f0db04ec42debbaf481d21a339be04e
ChermDev <carlos_e8246@hotmail.com>
2023-04-11 02:44:38 -0600

    Update text size to support smaller screens

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,12 +13,12 @@ function Home() {
                     <img className="rounded-full w-[15rem] h-[15rem] sm:w-[20rem] sm:h-[20rem] object-cover" src="https://res.cloudinary.com/practicaldev/image/fetch/s--a67XH0QN--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/xndmxrfhliweofif9jty.png" alt="" />
                 </div> */}
                 <div className="h-full flex flex-col justify-around items-center md:items-center md:justify-center from-teal-500 to-blue-300 dark:from-blue-400 dark:to-pink-400 bg-clip-text bg-gradient-to-r">
-                    <p className="w-full text-left text-2xl md:text-4xl text-stone-900 dark:text-white">Hello, I'm <strong className="text-transparent">Carlos Emmanuel Hernandez</strong>, SDET and Web Developer and I do</p>
+                    <p className="w-full text-left text-[4vw] sm:text-xl md:text-4xl text-stone-900 dark:text-white">Hello, I'm <strong className="text-transparent">Carlos Emmanuel Hernandez</strong>, SDET and Web Developer and I do</p>
                     <div className="w-min h-min [writing-mode:vertical-rl] md:[writing-mode:horizontal-tb]">
-                        <h1 className="[text-shadow:2px 2px 2px] text-[12.3vh] md:text-[17vw] leading-none text-left font-extrabold text-transparent">
+                        <h1 className="[text-shadow:2px 2px 2px] text-[11vh] sm:text-[12.3vh] md:text-[17vw] leading-none text-left font-extrabold text-transparent">
                             SOFTWARE
                         </h1>
-                        <h1 className="[text-shadow:2px 2px 2px] text-[9vh] md:text-[12.4vw] leading-none text-left font-extrabold text-transparent">
+                        <h1 className="[text-shadow:2px 2px 2px] text-[8vh] sm:text-[9vh] md:text-[12.4vw] leading-none text-left font-extrabold text-transparent">
                             DEVELOPMENT
                         </h1>
                     </div>

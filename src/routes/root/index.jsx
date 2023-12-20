036cb22e33ae7144af0c26d56ee8ae308644f4aa
CharlyyShell <chermdev@gmail.com>
2023-12-20 03:01:17 -0600

    ✨ feat: add root route

---
+++ src/routes/root/index.jsx
@@ -0,0 +1,15 @@
+import App from '../../pages/App'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+
+function Root() {
+    return (
+        <div>
+            <Navbar />
+            <App />
+            <Footer />
+        </div>
+    )
+}
+
+export default Root

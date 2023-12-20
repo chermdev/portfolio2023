6f1913974e60a41ab5ae62cc37cede0ce91f499b
CharlyyShell <chermdev@gmail.com>
2023-12-20 02:54:35 -0600

    🚚 chore: move App to pages/App

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react'
-import Work from './pages/Work'
-import Home2 from './pages/Home2'
-import { useDarkMode } from './context/ThemeContext'
-import { ReactComponent as BlobAnimation } from './assets/blob/blobanimation.svg'
-import { ReactComponent as BlobAnimationDark } from './assets/blob/blobanimation-dark.svg'
+import Work from './Work'
+import Home from './Home'
+import { useDarkMode } from '../context/ThemeContext'
+import { ReactComponent as BlobAnimation } from '../assets/blob/blobanimation.svg'
+import { ReactComponent as BlobAnimationDark } from '../assets/blob/blobanimation-dark.svg'
+
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
     return (
         <div className="App">
             <div className='relative bg-gradient-to-b from-transparent to-bgcolor overflow-hidden flex flex-col items-center'>
-                <Home2 />
+                <Home />
                 <div className='absolute z-[-1] top-16 overflow-hidden animate-[easeIn_1s_ease-in-out_0.3s_both]'>
                     {
                         darkMode ?
@@ -25,5 +26,6 @@ function App() {
         </div>
     )
 }
+
 
 export default App

855800e15b46fa713b1331b72d93c81868cc39e4
CharlyyShell <chermdev@gmail.com>
2023-12-21 03:31:46 -0600

    💄 style: fix Link button area

--- src/routes/coming-soon.jsx
+++ src/routes/coming-soon.jsx
@@ -2,27 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function ComingSoon() {
     return (
-        <main class="h-screen w-screen flex flex-col justify-center items-center  overflow-hidden bg-black">
+        <main className="h-screen w-screen flex flex-col justify-center items-center  overflow-hidden bg-black">
             <img src="avatar150.png" />
             <div className="relative flex items-center justify-center mt-4 mb-10">
-                <h1 class="text-7xl font-extrabold text-white text-center tracking-wide leading-[2.2rem]">Coming soon</h1>
-                <div class="bg-yellow-500 px-2 text-xl rounded text-black font-bold rotate-6 absolute bottom-[-35px]">
+                <h1 className="text-7xl font-extrabold text-white text-center tracking-wide leading-[2.2rem]">Coming soon</h1>
+                <div className="bg-yellow-500 px-2 text-xl rounded text-black font-bold rotate-6 absolute bottom-[-35px]">
                     Work in progress
                 </div>
             </div>
-            <button class="">
-                <a
-                    class="relative inline-block text-sm font-medium text-white/80 group active:text-orange-500 focus:outline-none focus:ring"
-                >
-                    <span
-                        class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-cyan-400group-hover:translate-y-0 group-hover:translate-x-0"
-                    ></span>
-
-                    <span class="relative block px-8 py-3 backdrop-blur-2xl bg-white/10 hover:bg-yellow-500/20 hover:text-yellow-500  border border-current rounded-2xl">
-                        <Link to="/">Go Home</Link>
-                    </span>
-                </a>
-            </button>
+            <Link className="relative block text-sm font-medium text-white/80 group active:text-yellow-400 focus:outline-none focus:ring px-8 py-3 backdrop-blur-2xl bg-white/10 hover:bg-yellow-500/20 hover:text-yellow-500  border border-current rounded-2xl">
+                Go Home
+            </Link>
         </main >
     )
 }
--- src/routes/error-page.jsx
+++ src/routes/error-page.jsx
@@ -5,27 +5,17 @@ export default function ErrorPage() {
     const error = useRouteError()
     console.error(error)
     return (
-        <main class="h-screen w-screen flex flex-col justify-center items-center overflow-hidden bg-black">
+        <main className="h-screen w-screen flex flex-col justify-center items-center overflow-hidden bg-black">
             <img src="avatar150.png" />
             <div className="relative flex items-center justify-center mt-10 mb-10">
-                <h1 class="text-9xl font-extrabold text-white text-center tracking-wide leading-[2.2rem]">404</h1>
-                <div class="bg-cyan-500 px-2 text-xl rounded text-black font-bold rotate-6 absolute bottom-[-35px]">
+                <h1 className="text-9xl font-extrabold text-white text-center tracking-wide leading-[2.2rem]">404</h1>
+                <div className="bg-cyan-500 px-2 text-xl rounded text-black font-bold rotate-6 absolute bottom-[-35px]">
                     Page Not Found
                 </div>
             </div>
-            <button class="">
-                <a
-                    class="relative inline-block text-sm font-medium text-white/80 group active:text-orange-500 focus:outline-none focus:ring"
-                >
-                    <span
-                        class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-cyan-400group-hover:translate-y-0 group-hover:translate-x-0"
-                    ></span>
-
-                    <span class="relative block px-8 py-3 backdrop-blur-2xl bg-white/10 hover:bg-cyan-400/20 hover:text-cyan-400 border border-current rounded-2xl">
-                        <Link to="/">Go Home</Link>
-                    </span>
-                </a>
-            </button>
+            <Link to="/" className="relative block text-sm font-medium text-white/80 group active:text-cyan-400 focus:outline-none focus:ring px-8 py-3 backdrop-blur-2xl bg-white/10 hover:bg-cyan-400/20 hover:text-cyan-400 border border-current rounded-2xl">
+                Go Home
+            </Link>
         </main >
     )
 }
